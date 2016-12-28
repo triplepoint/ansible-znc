5487126bfc3c4fd16243b9c7e00b204f2f8d7374
Jonathan Hanson <jonathan@jonathan-hanson.org>
2016-12-28 00:50:11 -0800

    Tweak the infratest a bit

--- tests/test_znc.py
+++ tests/test_znc.py
@@ -1,8 +1,23 @@
+from testinfra.utils.ansible_runner import AnsibleRunner
+
+testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
+
+
+def test_service_enabled(Service):
+    service = Service('znc')
+    assert service.is_enabled
+
+
 def test_service_running(Service):
     service = Service('znc')
     assert service.is_running
 
 
-def test_socket_listening(Socket):
-    socket = Socket('tcp://127.0.0.1:6666')
+def test_socket_listening_ipv4(Socket):
+    socket = Socket('tcp://0.0.0.0:6666')
     assert socket.is_listening
+
+
+def test_socket_listening_ipv6(Socket):
+    socket = Socket('tcp://:::6666')
+    assert not socket.is_listening
